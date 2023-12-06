f5a1cb52bd25005f5ab3489c4034d3ef7785ef72
Merovech <ari.roth@gmail.com>
2023-12-05 23:09:29 -0800

    Scaffolding for Days 5 and 6.

---
+++ Problems/Day5.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2023.Problems
+{
+	internal class Day5 : DayBase<double>
+	{
+		public Day5() : base(5) { }
+
+		public override double FirstProblem()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override double SecondProblem()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Problems/Day6.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2023.Problems
+{
+	internal class Day6 : DayBase<int>
+	{
+		public Day6() : base(6) { }
+
+		public override int FirstProblem()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int SecondProblem()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
